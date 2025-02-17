610ded65a4e0c5e1f04c6e42c919aa4e8292a9ce
Gerogy Talim <talimgeorgy@gmail.com>
2025-02-17 15:37:44 +0300

    Update GGP.java

--- GTGApp/src/main/java/org/example/GGP.java
+++ GTGApp/src/main/java/org/example/GGP.java
@@ -1,57 +1,84 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
- class GTGApp {
-     public static void main(String[] args) {
-         JFrame jf = new JFrame();
-         jf.setTitle("GTG");
-         jf.setSize(300, 250);
-         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class GTGApp {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setTitle("GTG");
+        jf.setSize(300, 250);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-         JMenuBar jmb = new JMenuBar();
-         jf.setJMenuBar(jmb);
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.PINK);
 
-         JMenu application = new JMenu("Application");
-         jmb.add(application);
+        JPanel northPanel = new JPanel();
+        northPanel.setBackground(Color.RED);
+        jf.add(northPanel, BorderLayout.NORTH);
 
-         JMenuItem open = new JMenuItem("Open ...");
-         application.add(open);
-         application.addSeparator();
-         JMenuItem close = new JMenuItem("Close");
-         application.add(close);
+        JPanel southPanel = new JPanel();
+        southPanel.setBackground(Color.BLACK);
+        jf.add(southPanel, BorderLayout.SOUTH);
 
-         JMenu edit = new JMenu("Edit");
-         jmb.add(edit);
-         JMenuItem finish = new JMenuItem("Finish");
-         edit.add(finish);
+        JPanel eastPanel = new JPanel();
+        eastPanel.setBackground(Color.BLUE);
+        jf.add(eastPanel, BorderLayout.EAST);
 
-         JMenu view = new JMenu("View");
-         jmb.add(view);
-         JMenuItem viewed = new JMenuItem("Viewed");
-         view.add(viewed);
+        JPanel westPanel = new JPanel();
+        westPanel.setBackground(Color.YELLOW);
+        jf.add(westPanel, BorderLayout.WEST);
 
-         JMenu navigate = new JMenu("Navigate");
-         jmb.add(navigate);
-         JMenuItem done = new JMenuItem("Done");
-         navigate.add(done);
+        JPanel centerPanel = new JPanel();
+        centerPanel.setBackground(Color.PINK);
+        jf.add(centerPanel, BorderLayout.CENTER);
+        centerPanel.setLayout(null);
 
-         JMenu about = new JMenu("About");
-         jmb.add(about);
-         JMenuItem exit = new JMenuItem("Exit");
-         about.add(exit);
 
-         jf.setVisible(true);
+        jf.add(panel);
+        JMenuBar jmb = new JMenuBar();
+        jf.setJMenuBar(jmb);
 
-         open.addActionListener(new ActionListener() {
-             @Override
-             public void actionPerformed(ActionEvent e) {
-                 org.example.Registration register = new org.example.Registration();
-                 register.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                 register.setVisible(true);
-             }
-         });
-     }
- }
+        JMenu application = new JMenu("Application");
+        jmb.add(application);
+
+        JMenuItem open = new JMenuItem("Open ...");
+        application.add(open);
+        application.addSeparator();
+        JMenuItem close = new JMenuItem("Close");
+        application.add(close);
+
+        JMenu edit = new JMenu("Edit");
+        jmb.add(edit);
+        JMenuItem finish = new JMenuItem("Finish");
+        edit.add(finish);
+
+        JMenu view = new JMenu("View");
+        jmb.add(view);
+        JMenuItem viewed = new JMenuItem("Viewed");
+        view.add(viewed);
+
+        JMenu navigate = new JMenu("Navigate");
+        jmb.add(navigate);
+        JMenuItem done = new JMenuItem("Done");
+        navigate.add(done);
+
+        JMenu about = new JMenu("About");
+        jmb.add(about);
+        JMenuItem exit = new JMenuItem("Exit");
+        about.add(exit);
+
+        jf.setVisible(true);
+
+        open.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Registration register = new Registration();
+                register.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                register.setVisible(true);
+            }
+        });
+    }
+}
